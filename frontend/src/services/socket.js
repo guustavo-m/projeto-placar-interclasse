a67a8175b1d33fad5fb2e5a55681f2ee0c18e19d
o guti <gustavo.millamonte10@gmail.com>
2026-06-23 08:16:03 -0300

    feat: enhance socket.js structure for improved readability and maintainability

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,10 +1,18 @@
-import { io }
-from "socket.io-client";
+import { io } from "socket.io-client";
 
-const socket =
-    io(
-        import.meta.env
-            .VITE_API_URL
-    );
+const socket = io(
+    import.meta.env.VITE_API_URL,
+    {
+        transports: ["websocket"]
+    }
+);
+
+socket.on("connect", () => {
+    console.log("SOCKET CONECTADO", socket.id);
+});
+
+socket.on("connect_error", err => {
+    console.log("ERRO SOCKET", err);
+});
 
 export default socket;
